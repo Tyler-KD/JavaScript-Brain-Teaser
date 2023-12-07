76acaf5bec0a8a087a51e14de7fd05e341a75e6e
unknown <tyler.kd.knapp@gmail.com>
2023-12-07 09:03:43 -0600

    Added quiz box, answer buttons, styling, and questions

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-console.log ("This is working!");
+const questions = [
+    {
+    question : "Which of these values is a string?",
+    answers: [
+        {text: "true", correct: false}, 
+        {text: "'33'", correct: true}, 
+        {text: "33", correct: false}, 
+        {text: "Howdy", correct: false}, 
+    ]
+},
+{
+    question : "What is the correct way to store data within an array?",
+    answers: [
+        {text: "var names = [Andre, Karl, Rashida, Olivia]", correct: false}, 
+        {text: "var names = ('Andre', 'Karl', 'Rashida', 'Olivia')", correct: false}, 
+        {text: "var names = ['Andre', 'Karl', 'Rashida', 'Olivia']", correct: true}, 
+        {text: "None of the above", correct: false}, 
+    ]
+},
+{
+    question : "Which method removes the last element in the array?",
+    answers: [
+        {text: "unshift()", correct: false}, 
+        {text: "pop()", correct: true}, 
+        {text: "push()", correct: false}, 
+        {text: "slice()", correct: false}, 
+    ]
+},
+{
+    question : "How do you write an 'if' statement if 'i' is no greater than 30?",
+    answers: [
+        {text: "i <= 30", correct: true}, 
+        {text: "i < 30", correct: false}, 
+        {text: "i <> 30", correct: false}, 
+        {text: "i => 30", correct: false}, 
+    ]
+},
+
+];
+
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-buttons");
