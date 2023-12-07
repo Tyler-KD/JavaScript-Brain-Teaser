1c5ed96a57e41270d5a99e96e0e0a2d93bf57d35
unknown <tyler.kd.knapp@gmail.com>
2023-12-07 15:17:25 -0600

    Updated start quiz function

--- script.js
+++ script.js
@@ -1,68 +1,66 @@
 const questions = [
     {
-    question : "Which of these values is a string?",
-    answers: [
-        {text: "true", correct: false}, 
-        {text: "'33'", correct: true}, 
-        {text: "33", correct: false}, 
-        {text: "Howdy", correct: false}, 
-    ]
-},
-{
-    question : "What is the correct way to store data within an array?",
-    answers: [
-        {text: "var names = [Andre, Karl, Rashida, Olivia]", correct: false}, 
-        {text: "var names = ('Andre', 'Karl', 'Rashida', 'Olivia')", correct: false}, 
-        {text: "var names = ['Andre', 'Karl', 'Rashida', 'Olivia']", correct: true}, 
-        {text: "None of the above", correct: false}, 
-    ]
-},
-{
-    question : "Which method removes the last element in the array?",
-    answers: [
-        {text: "unshift()", correct: false}, 
-        {text: "pop()", correct: true}, 
-        {text: "push()", correct: false}, 
-        {text: "slice()", correct: false}, 
-    ]
-},
-{
-    question : "How do you write an 'if' statement if 'i' is no greater than 30?",
-    answers: [
-        {text: "i <= 30", correct: true}, 
-        {text: "i < 30", correct: false}, 
-        {text: "i <> 30", correct: false}, 
-        {text: "i => 30", correct: false}, 
-    ]
-},
-
+        question: "Which of these values is a string?",
+        options: ["true", "'33'", "33", "Howdy"],
+        answer: "'33'"
+    },
+    {
+        question: "What is the correct way to store data within an array?",
+        options: ["var names = [Andre, Karl, Rashida, Olivia]", "var names = ('Andre', 'Karl', 'Rashida', 'Olivia')", 
+        "var names = ['Andre', 'Karl', 'Rashida', 'Olivia']", "None of the above"],
+        answer: "var names = ['Andre', 'Karl', 'Rashida', 'Olivia']"
+    },
+    {
+        question: "Which method removes the last element in the array?",
+        options: ["unshift()", "pop()", "push()", "slice()"],
+        answer: "pop()"
+    },
+    {
+        question: "How do you write an 'if' statement if 'i' is no greater than 30?",
+        options: ["i <= 30", "i < 30", "i <> 30", "i => 30"],
+        answer: "i <= 30"
+    }
 ];
-
-const questionElement = document.getElementById("question");
-const answerButton = document.getElementById("answer-buttons");
-const nextButton = document.getElementById("next-btn");
 
 let currentQuestionIndex = 0;
 let score = 0;
 
-function startQuiz(){
-    currentQuestionIndex = 0;
-    score = 0;
-    nextButton.innerHTML = "Next";
-    showQuestion();
-}
+const questionElement = document.getElementById('question');
+const optionsElement = document.getElementById('options');
+const scoreElement = document.getElementById('score');
 
-function showQuestion(){
-    let currentQuestion = questions[currentQuestionIndex];
-    let questionNo = currentQuestionIndex + 1;
-    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+function displayQuestion() {
+    const currentQuestion = questions[currentQuestionIndex];
+    questionElement.textContent = currentQuestion.question;
+    optionsElement.innerHTML = '';
 
-    currentQuestion.answers.forEach(answer => {
-        const button = document.createElement("button");
-        button.innerHTML = answer.text;
-        button.classList.add("btn");
-        answerButton.appendChild(button);
+    currentQuestion.options.forEach((option, Index) => {
+        const button = document.createElement('button');
+        button.textContent = option;
+        button.addEventListener('click', () => checkAnswer(option));
+        optionsElement.appendChild(button);
     });
 }
 
-startQuiz();
+function checkAnswer(selectedOption) {
+    const currentQuestion = questions[currentQuestionIndex];
+    if (selectedOption === currentQuestion.answer) {
+        score++;
+    }
+
+    currentQuestionIndex++;
+
+    if (currentQuestionIndex < questions.length) {
+        displayQuestion();
+    } else {
+        showResult();
+    }
+}
+
+function showResult() {
+    questionElement.textContent = 'Quiz Completed!';
+    optionsElement.innerHTML = '';
+    scoreElement.textContent = `Your Score: ${score} / ${questions.length}`;
+}
+
+displayQuestion();
